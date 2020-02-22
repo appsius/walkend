ba6ac3e5afe9508fb4fa89bcddd6c9bdaab677e2
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-22 21:36:16 +0300

    Adding Keyboard Controls

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body } = Matter;
 const cells = 15;
 const width = 600;
 const height = 600;
@@ -168,16 +168,18 @@ const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
 World.add(world, ball);
 
 document.addEventListener('keydown', event => {
+  const { x, y } = ball.velocity;
+
   if (event.keyCode === 87) {
-    console.log('Move up!!!');
+    Body.setVelocity(ball, { x, y, y: -5 });
   }
   if (event.keyCode === 68) {
-    console.log('Move right!!!');
+    Body.setVelocity(ball, { x: x + 5, y });
   }
   if (event.keyCode === 83) {
-    console.log('Move down!!!');
+    Body.setVelocity(ball, { x, y: y + 5 });
   }
   if (event.keyCode === 65) {
-    console.log('Move left!!!');
+    Body.setVelocity(ball, { x: x - 5, y });
   }
 });
