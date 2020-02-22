c90a2c4e42f6cad66a33f9209e659a107b932b20
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-22 16:49:58 +0300

    Drawing Horizontal Segments

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cells = 3;
 const width = 600;
 const height = 600;
 
+const unitLength = width / cells;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
@@ -111,12 +113,21 @@ stepThroughCell = (row, column) => {
 
 stepThroughCell(startRow, startColumn);
 
-horizontals.forEach(row => {
-  row.forEach(open => {
+horizontals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
     if (open) {
       return;
     }
 
-    const wall = Bodies.rectangle();
+    const wall = Bodies.rectangle(
+      columnIndex * unitLength + unitLength / 2,
+      rowIndex * unitLength + unitLength,
+      unitLength,
+      10,
+      {
+        isStatic: true
+      }
+    );
+    World.add(world, wall);
   });
 });
